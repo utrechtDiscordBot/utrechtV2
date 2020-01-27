50c42be6506f18eb4a32f49dd8fda44a29ba3ade
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-01-27 20:27:30 +0100

    init

---
+++ commands/unmute.js
@@ -0,0 +1,23 @@
+const discord = require("discord.js");
+const ms = require("ms");
+
+module.exports.run = async (bot, message, arguments) => {
+
+    // !mute gebruiker tijd
+
+    var user = message.guild.member(message.mentions.users.first() || message.guild.members.get(arguments[0]));
+
+    var Role = message.guild.roles.find("name", "Muted");
+    var Role2 = message.guild.roles.find("name", "Speler");
+
+    setTimeout(function() {
+        user.removeRole(Role.id);
+        user.addRole(Rold2.id);
+
+});
+}
+
+module.exports.help = {
+    name: "unmute",
+    description: "unMute iemand voor een bepaalde tijd."
+}
