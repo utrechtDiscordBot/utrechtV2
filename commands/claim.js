ef6270a1fac4e3ad922a92ab891501556528e95c
utrechtDiscordBot <59834711+utrechtDiscordBot@users.noreply.github.com>
2020-02-13 16:11:24 +0100

    init

--- commands/claim.js
+++ commands/claim.js
@@ -18,7 +18,7 @@ module.exports.run = async (bot, message, args) => {
 
     var embedClaimTicket = new discord.RichEmbed()
         .setTitle("Hoi, " + message.channel.name)
-        .setDescription(`Je ticket is geclaimed door: ${message.author}`)
+        .addField("Je ticket is geclaimed door: ", message.author)
         .setColor("#ffa500")
         .setThumbnail(icon)
         .setFooter("ticket geclaimed");
